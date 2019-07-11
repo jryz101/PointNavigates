030e054959de0b5537e14cd976deb32e26cf0f10
JerryTan <jryz101@gmail.com>
2019-07-12 07:27:43 +0800

    1. Installed pod Mapbox, and imported Mapbox, MapboxCoreNavigation, MapboxNavigation, MapboxDirections framework.

--- PointNavigates/ViewController.swift
+++ PointNavigates/ViewController.swift
@@ -1,20 +1,21 @@
-//
 //  ViewController.swift
 //  PointNavigates
-//
 //  Created by Jerry Tan on 12/07/2019.
 //  Copyright © 2019 Starknet Technologies®. All rights reserved.
-//
+
 
 import UIKit
+import Mapbox
+import MapboxCoreNavigation
+import MapboxNavigation
+import MapboxDirections
+
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
