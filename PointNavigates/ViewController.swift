6806cf867be09fd32f7d1108a9432c9022fdd16f
JerryTan <jryz101@gmail.com>
2019-07-12 06:52:49 +0800

    Initial Commit

---
+++ PointNavigates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PointNavigates
+//
+//  Created by Jerry Tan on 12/07/2019.
+//  Copyright © 2019 Starknet Technologies®. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
