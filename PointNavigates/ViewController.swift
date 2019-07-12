e8f53f2ec4a19eb32aac6dc7e98dfb43aa5f1c67
JerryTan <jryz101@gmail.com>
2019-07-12 17:18:14 +0800

    3. Declared MGLMapViewDelegate protocol, NavigationMapView subclass, set mapView bounds rectangle, and adds to the subView.

--- PointNavigates/ViewController.swift
+++ PointNavigates/ViewController.swift
@@ -11,11 +11,22 @@ import MapboxNavigation
 import MapboxDirections
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MGLMapViewDelegate {
+    
+    
+    
+    //NavigationMapView is a subclass of MGLMapView with convenience functions for adding Route lines to a map
+    var mapView: NavigationMapView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //The bounds rectangle, which describes the view’s location and size in its own coordinate system
+        mapView = NavigationMapView(frame: view.bounds)
+        //Adds a view to the end of the receiver’s list of subviews
+        view.addSubview(mapView)
+        
         
     }
 }
